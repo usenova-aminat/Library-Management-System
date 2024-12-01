c4e46db7023b2aaa17c9300fcd54dd3132260f55
usenova-aminat <usenova624@gmail.com>
2024-12-01 20:12:34 +0600

    Add files via upload
    Authorization page(Sign in/ Sign up)
    Profile page(My purchases)

--- HelloApplication.java
+++ HelloApplication.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 1000);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("authorization.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 500, 500);
+        stage.setTitle("Authorization");
         stage.setScene(scene);
         stage.show();
     }
--- HelloController.java
+++ HelloController.java
@@ -4,5 +4,11 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class HelloController {
+    @FXML
+    private Label welcomeText;
 
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
 }
